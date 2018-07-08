6ab81e02f2a8da532ae50c30bf8809e21bbbc60c
Joel Rudsberg <rudsberg@live.se>
2018-07-08 18:03:06 +0200

    bug fix

--- Views/StartPage/StartPage.js
+++ Views/StartPage/StartPage.js
@@ -18,8 +18,6 @@ export default class StartPage extends Component {
         <ScrollView>
           <CityHeader city={'Göteborg'} />
           <Warning message={'1 risk för västra Götalands län, Bohuslän och Göteborg.'} />
-          <DayForecast day={'Söndag 8 juli'} degrees={25} weatherType={'Soligt'} amountRain={0} />
-          <ForecastHours />
           <DayForecast
             day={'Måndag 9 juli'}
             degrees={25}
