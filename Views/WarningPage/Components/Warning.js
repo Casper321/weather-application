5eee6b96396b067a27973f7bcf1f77065fe55e61
CarlClaesson <claesson.carl@hotmail.com>
2018-07-22 14:54:32 +0200

    fixade mer på VarningPage, inte klar än dock

---
+++ Views/WarningPage/Components/Warning.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Text, View, StyleSheet } from 'react-native'
+import { FontAwesome, MaterialCommunityIcons, Feather } from '@expo/vector-icons/'
+import * as style from '../../../Assets/style'
+import s from '../../../Assets/style'
+
+const Warning = ({ message = 'Ingen varning utfärdad.' , typeOfWarning = 'fire'}) => {
+  return (
+    <View style={[s.br, s.w, s.mlA, s.mrA, s.pa2, s.mb3, s.mt1, s.col_bg_white, s.flexDr, s.flexJsb, s.flexAce]}>
+      <View style={[styles.warningIconContainer, s.flex2]}>
+        <View style={[styles.center, styles.flexOne]}>
+          <FontAwesome name='circle-thin' color={style.COL_WARNING} size={50} />
+        </View>
+        <View>
+          <MaterialCommunityIcons
+            style={[styles.warningFire, s.abs]}
+            name= {typeOfWarning}
+            color={style.COL_PRIMARY}
+            size={style.ICON_SIZE_SMALL}
+          />
+        </View>
+      </View>
+      <Text style={[s.flex8]}>
+        {message}
+      </Text>
+      <View style={[s.flexOne, s.flexDr, s.flexJfe, s.flexAce]}>
+        <Feather name='chevron-right' color={style.COL_PRIMARY} size={style.ICON_SIZE_MEDIUM} />
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  warningIconContainer: {
+    height: 50,
+    width: 50
+  },
+  warningFire: {
+    top: -37,
+    left: 10
+  }
+})
+
+ /*
+    Behöver ta in en prop "typeOfWarning" som byter icon beroende på vilken varning det är, just nu fungerar den endast för eld.
+    */
+export default Warning
--- Views/WarningPage/WarningPage.js
+++ Views/WarningPage/WarningPage.js
@@ -1,40 +1,26 @@
 import React, { Component } from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, View, StyleSheet, ScrollView} from 'react-native'
 import Container from '../../Components/Container'
 import Header from '../../Components/Header'
 import { FontAwesome, MaterialCommunityIcons, Feather } from '@expo/vector-icons/'
 import * as style from '../../Assets/style'
 import s from '../../Assets/style'
+import Warning from './Components/Warning'
 
 export default class WarningPage extends Component {
   render () {
     return (
       <Container>
         <Header />
-        <View style={[s.br, s.w, s.mlA, s.mrA, s.pa2, s.mb3, s.mt1, s.col_bg_white, s.flexDr, s.flexJsb, s.flexAce]}>
-      <View style={[styles.warningIconContainer, s.flex2]}>
-        <View style={[styles.center, styles.flexOne]}>
-          <FontAwesome name='circle-thin' color={style.COL_WARNING} size={50} />
-        </View>
-        <View>
-          <MaterialCommunityIcons
-            style={[styles.warningFire, s.abs]}
-            name='fire'
-            color={style.COL_PRIMARY}
-            size={style.ICON_SIZE_SMALL}
-          />
-        </View>
-      </View>
-      <Text style={[s.flex8]}>
-        DISPLAY WARNING HERE
-      </Text>
-      <View style={[s.flexOne, s.flexDr, s.flexJfe, s.flexAce]}>
-        <Feather name='chevron-right' color={style.COL_PRIMARY} size={style.ICON_SIZE_MEDIUM} />
-      </View>
-    </View>
+        <ScrollView>
+          <Warning message={'DISPLAY WARNING HERE'} typeOfWarning={'fire'} />
+          <Warning message={'DISPLAY WARNING HERE'} typeOfWarning={'weather-snowy'} />
+          <Warning message={'DISPLAY WARNING HERE'} typeOfWarning={'weather-rainy'} />
+          <Warning message={'DISPLAY WARNING HERE'} typeOfWarning={'weather-windy'} />
+        </ScrollView>
       </Container>
     )
-  
+  // fire, weather-snowy, weather-rainy, weather-windy
   }
 }
 
@@ -49,3 +35,6 @@ const styles = StyleSheet.create({
   }
 })
 
+/*
+    Behöver fixa så att message är varningen man får från Api och sedan mappa varningarna samt väljer rätt typeOfWarning
+    */
