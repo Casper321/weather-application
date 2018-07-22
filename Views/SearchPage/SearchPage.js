19fdf364bfdb99a5f3500a7965450392953e5e3a
Joel Rudsberg <rudsberg@live.se>
2018-07-22 23:08:46 +0200

    imports

--- Views/SearchPage/SearchPage.js
+++ Views/SearchPage/SearchPage.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
-import { Text, View, TextInput, StyleSheet, ScrollView, TouchableNativeFeedback } from 'react-native'
+import { Text, View, TextInput, StyleSheet, ScrollView, FlatList, TouchableHighlight } from 'react-native'
 import { SearchBar } from 'react-native-elements'
 import Container from '../../Components/Container'
 import Header from '../../Components/Header'
 import SearchItem from './Components/SearchItem'
 import BoxContainer from '../../Components/BoxContainer'
+import style from '../../Assets/style'
+import * as s from '../../Assets/style'
 
 export default class SearchPage extends Component {
   state = {
@@ -39,22 +41,20 @@ export default class SearchPage extends Component {
     request.send(null)
   }
 
+  onCityPicked = city => {
+    console.log(city)
+  }
+
   render () {
     const { citiesAvailable } = this.state
-    let citiesList
+    let citiesList = []
 
     if (citiesAvailable.length >= 1) {
       citiesList = citiesAvailable.map(city => {
-        const cityName = city.city.substring(0, city.city.indexOf(','))
-        const longerLocationName = city.city.substring(city.city.indexOf(',') + 2, city.city.length)
-
-        if (city && city !== undefined) {
-          return (
-            <TouchableNativeFeedback onPress={console.log(cityName)}>
-              <SearchItem city={cityName} longerLocationName={longerLocationName} key={Math.random() * 1000} />
-            </TouchableNativeFeedback>
-          )
-        }
+        let cityObj = {}
+        cityObj.cityName = city.city.substring(0, city.city.indexOf(','))
+        cityObj.longerLocationName = city.city.substring(city.city.indexOf(',') + 2, city.city.length)
+        return cityObj
       })
     }
 
@@ -65,7 +65,17 @@ export default class SearchPage extends Component {
           icon={{ type: 'font-awesome', name: 'search' }}
           placeholder='Skriv din ort här...'
         />
-        {citiesAvailable && <ScrollView><BoxContainer>{citiesList}</BoxContainer></ScrollView>}
+        {citiesAvailable
+          ? <FlatList
+            data={citiesList}
+            keyExtractor={() => Math.random() * 1000}
+            renderItem={({ city }) => (
+              <TouchableHighlight underlayColor={style.COL_GREY} onPress={() => this.onCityPicked(city)}>
+                <SearchItem city={city.cityName} longerLocationName={city.longerLocationName} />
+              </TouchableHighlight>
+              )}
+            />
+          : null}
       </Container>
     )
   }
