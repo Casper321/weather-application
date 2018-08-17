4332914f504185b04c475194e665c4d7e9b250b2
Casper321 <CLindberg97@gmail.com>
2018-08-17 21:37:21 +0200

    Fixed icons

--- Assets/Functions/getWeatherCondition.js
+++ Assets/Functions/getWeatherCondition.js
@@ -1,112 +1,111 @@
 export default (getWeatherType = value => {
-  value = value - 1
   let weatherCondition = null
   switch (value) {
-    case 0:
+    case 1:
       weatherCondition = 'Klar himmel'
       break
 
-    case 1:
-      weatherCondition = 'Halvklar himmel'
-      break
-
     case 2:
-      weatherCondition = 'Varierande molnighet'
+      weatherCondition = 'Nästan klar himmel'
       break
 
     case 3:
-      weatherCondition = 'Halvklar himmel'
+      weatherCondition = 'Varierande molnighet'
       break
 
     case 4:
-      weatherCondition = 'Molnig himmel'
+      weatherCondition = 'Halvklar himmel'
       break
 
     case 5:
-      weatherCondition = 'Mulet'
+      weatherCondition = 'Molnig himmel'
       break
 
     case 6:
-      weatherCondition = 'Dimma'
+      weatherCondition = 'Mulet'
       break
 
     case 7:
-      weatherCondition = 'Lätta regnskurar'
+      weatherCondition = 'Dimma'
       break
 
     case 8:
-      weatherCondition = 'Måttliga regnskurar'
+      weatherCondition = 'Lätta regnskurar'
       break
 
     case 9:
-      weatherCondition = 'Kraftiga regnskurar'
+      weatherCondition = 'Måttliga regnskurar'
       break
 
     case 10:
-      weatherCondition = 'Åska'
+      weatherCondition = 'Kraftiga regnskurar'
       break
 
     case 11:
-      weatherCondition = 'Lätta skurar snöblandat regn'
-      break
-
-    case 12:
-      weatherCondition = 'Måttliga skurar snöblandat regn'
-      break
-
-    case 13:
-      weatherCondition = 'Kraftiga skurar snöblandat regn'
-      break
-
-    case 14:
-      weatherCondition = 'Lätta snöbyar'
-      break
-
-    case 15:
-      weatherCondition = 'Måttliga snöbyar'
-      break
-
-    case 16:
-      weatherCondition = 'Kraftiga snöbyar'
-      break
-
-    case 17:
-      weatherCondition = 'Lätt regn'
-      break
-
-    case 18:
-      weatherCondition = 'Måttligt regn'
-      break
-
-    case 19:
-      weatherCondition = 'Kraftigt regn'
-      break
-
-    case 20:
       weatherCondition = 'Åska'
       break
 
+    case 12:
+      weatherCondition = 'Lätta skurar snöblandat regn'
+      break
+
+    case 13:
+      weatherCondition = 'Måttliga skurar snöblandat regn'
+      break
+
+    case 14:
+      weatherCondition = 'Kraftiga skurar snöblandat regn'
+      break
+
+    case 15:
+      weatherCondition = 'Lätta snöbyar'
+      break
+
+    case 16:
+      weatherCondition = 'Måttliga snöbyar'
+      break
+
+    case 17:
+      weatherCondition = 'Kraftiga snöbyar'
+      break
+
+    case 18:
+      weatherCondition = 'Lätt regn'
+      break
+
+    case 19:
+      weatherCondition = 'Måttligt regn'
+      break
+
+    case 20:
+      weatherCondition = 'Kraftigt regn'
+      break
+
     case 21:
-      weatherCondition = 'Lätt snöblandat regn'
+      weatherCondition = 'Åska'
       break
 
     case 22:
-      weatherCondition = 'Måttligt snöblandat regn'
+      weatherCondition = 'Lätt snöblandat regn'
       break
 
     case 23:
-      weatherCondition = 'Kraftigt snöblandat regn'
+      weatherCondition = 'Måttligt snöblandat regn'
       break
 
     case 24:
-      weatherCondition = 'Lätt snöfall'
+      weatherCondition = 'Kraftigt snöblandat regn'
       break
 
     case 25:
-      weatherCondition = 'Måttligt snöfall'
+      weatherCondition = 'Lätt snöfall'
       break
 
     case 26:
+      weatherCondition = 'Måttligt snöfall'
+      break
+
+    case 27:
       weatherCondition = 'Kraftigt snöfall'
       break
 
