2ba8b8d39b35d41fffe05ac354bb4afc4a3f3b89
Joel Rudsberg <rudsberg@live.se>
2018-08-26 10:53:47 +0200

    swipeable drawer

--- config/router.js
+++ config/router.js
@@ -1,74 +1,111 @@
-import React from 'react'
-import { Platform } from 'react-native'
-import { createBottomTabNavigator, createStackNavigator, createDrawerNavigator, DrawerItems } from 'react-navigation'
+import React, { Component } from 'react'
+import {
+  Platform,
+  ScrollView,
+  SafeAreaView,
+  StyleSheet,
+  View,
+  AsyncStorage,
+  Text,
+  TouchableOpacity,
+  TouchableWithoutFeedback
+} from 'react-native'
+import {
+  createBottomTabNavigator,
+  createStackNavigator,
+  createDrawerNavigator,
+  DrawerItems,
+  createMaterialTopTabNavigator
+} from 'react-navigation'
 import StartPage from '../Views/StartPage/StartPage'
+import SearchPage from '../Views/SearchPage/SearchPage'
 import InfoPage from '../Views/InfoPage/InfoPage'
 import TenDaysForecastPage from '../Views/TenDaysForecastPage/TenDaysForecastPage'
+import AllHoursForecastPage from '../Views/AllHoursForecastPage/AllHoursForecastPage'
 import WarningPage from '../Views/WarningPage/WarningPage'
 import AnalysisPage from '../Views/AnalysisPage/AnalysisPage'
-import SettingsPage from '../Views/SettingsPage/SettingsPage'
 import { FontAwesome, SimpleLineIcons, Entypo, MaterialCommunityIcons } from '@expo/vector-icons'
+import s from '../Assets/style'
 import * as style from '../Assets/style'
+import fetchWeatherData from '../Assets/Functions/fetchWeatherData'
+import { connect } from 'react-redux'
 import CustomDrawerContentComponent from './Components/CustomDrawerContentComponent'
 
-export const Tabs = createBottomTabNavigator(
+const DRAWER_INACTIVE_TINT_COLOR = '#000'
+
+export const Tabs = createMaterialTopTabNavigator(
   {
-    Start: StartPage,
-    Långprognos: TenDaysForecastPage,
-    Varningar: WarningPage,
-    Dataanalys: AnalysisPage
+    Start: {
+      screen: StartPage,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor, focused }) => (
+          <FontAwesome
+            name={'sun-o'}
+            size={style.ICON_SIZE_SMALL}
+            color={focused ? tintColor : DRAWER_INACTIVE_TINT_COLOR}
+          />
+        )
+      }
+    },
+    Långprognos: {
+      screen: TenDaysForecastPage,
+      navigationOptions: {
+        tabBarLabel: '10 dagar',
+        tabBarIcon: ({ tintColor, focused }) => (
+          <SimpleLineIcons
+            name={'social-soundcloud'}
+            size={style.ICON_SIZE_SMALL}
+            color={focused ? tintColor : DRAWER_INACTIVE_TINT_COLOR}
+          />
+        )
+      }
+    },
+    Varningar: {
+      screen: WarningPage,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor, focused }) => (
+          <FontAwesome
+            name={'warning'}
+            size={style.ICON_SIZE_SMALL}
+            color={focused ? tintColor : DRAWER_INACTIVE_TINT_COLOR}
+          />
+        )
+      }
+    },
+    Dataanalys: {
+      screen: AnalysisPage,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor, focused }) => (
+          <Entypo
+            name={'line-graph'}
+            size={style.ICON_SIZE_SMALL}
+            color={focused ? tintColor : DRAWER_INACTIVE_TINT_COLOR}
+          />
+        )
+      }
+    }
   },
   {
-    order: ['Start', 'Långprognos', 'Varningar', 'Dataanalys'],
+    tabBarPosition: 'bottom',
     animationEnabled: true,
-    navigationOptions: ({ navigation }) => ({
-      tabBarIcon: ({ focused, tintColor, inactiveTintColor }) => {
-        const { routeName } = navigation.state
-        let icon = null
-        if (routeName === 'Start') {
-          icon = (
-            <FontAwesome name={'sun-o'} size={style.ICON_SIZE_SMALL} color={focused ? tintColor : inactiveTintColor} />
-          )
-        } else if (routeName === 'Långprognos') {
-          icon = (
-            <SimpleLineIcons
-              name={'social-soundcloud'}
-              size={style.ICON_SIZE_SMALL}
-              color={focused ? tintColor : inactiveTintColor}
-            />
-          )
-        } else if (routeName === 'Varningar') {
-          icon = (
-            <FontAwesome
-              name={'warning'}
-              size={style.ICON_SIZE_SMALL}
-              color={focused ? tintColor : inactiveTintColor}
-            />
-          )
-        } else if (routeName === 'Dataanalys') {
-          icon = (
-            <Entypo name={'line-graph'} size={style.ICON_SIZE_SMALL} color={focused ? tintColor : inactiveTintColor} />
-          )
-        } else if (routeName === 'Långprognos') {
-          icon = (
-            <MaterialCommunityIcons
-              name={'radar'}
-              size={style.ICON_SIZE_SMALL}
-              color={focused ? tintColor : inactiveTintColor}
-            />
-          )
-        }
-
-        return icon
-      }
-    }),
+    swipeEnabled: true,
+    optimizationsEnabled: true,
     tabBarOptions: {
-      tintColor: style.COL_GOOGLE_BLUE,
-      inactiveTintColor: style.COL_DARK_GREY,
+      showIcon: true,
+      activeTintColor: style.COL_GOOGLE_BLUE,
+      inactiveTintColor: style.COL_BLACK,
+      upperCaseLabel: false,
       style: {
+        height: 60,
         backgroundColor: style.COL_WHITE,
         borderTopWidth: 0,
         elevation: 20
+      },
+      labelStyle: {
+        fontSize: style.FONT_SIZE_XS - 1
+      },
+      indicatorStyle: {
+        backgroundColor: style.COL_GOOGLE_BLUE
       }
     }
   }
@@ -93,30 +130,10 @@ export const InfoStack = createStackNavigator(
   }
 )
 
-export const SettingsStack = createStackNavigator(
-  {
-    Inställningar: {
-      screen: SettingsPage,
-      navigationOptions: {
-        tabBarVisible: false,
-        title: 'Inställningar',
-        headerStyle: { backgroundColor: style.COL_GOOGLE_BLUE },
-        headerTintColor: '#fff'
-      }
-    }
-  },
-  {
-    initialRouteName: 'Inställningar',
-    headerMode: Platform.OS === 'ios' ? 'float' : 'screen',
-    headerMode: 'none'
-  }
-)
-
 export const Drawer = createDrawerNavigator(
   {
     Väderprognos: Tabs,
     Information: InfoPage
-    /* Inställningar: SettingsPage */
   },
   {
     contentComponent: props => <CustomDrawerContentComponent {...props} />,
